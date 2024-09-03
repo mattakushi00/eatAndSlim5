76c393d459a1e1b4f02c2158a59e648a65bfc3d8
m.gromov <gromoffmaximka@gmail.com>
2024-09-03 17:03:00 +0300

    hide header in mobile

--- src/js/header.js
+++ src/js/header.js
@@ -1,21 +1,6 @@
 const header = document.querySelector('.header')
 const face = document.querySelector('.main')
 const activeHeader = 'header_active'
-const breakPoint = 1024
-const mediaQuery = matchMedia(`(max-width: ${breakPoint}px)`)
-const setOpacity = () => window.scrollY > face.clientHeight ?
-    header.classList.add(activeHeader) :
-    header.classList.remove(activeHeader)
-const checkWidth = () => {
-    const isMobile = window.innerWidth <= breakPoint
-    if (isMobile) {
-        header.classList.add(activeHeader)
-        window.removeEventListener('scroll', setOpacity, { passive: true })
-        return
-    }
-    header.classList.remove(activeHeader)
-    window.addEventListener('scroll', setOpacity, { passive: true })
-}
-
-checkWidth()
-mediaQuery.addEventListener('change', checkWidth)
+const setOpacity = () => window.scrollY > face.clientHeight ? header.classList.add(activeHeader) : header.classList.remove(activeHeader)
+setOpacity()
+window.addEventListener('scroll', setOpacity, { passive: true })
