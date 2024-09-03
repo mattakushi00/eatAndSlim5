0d0b418aa42592e3f9956f1615d91377dff89edb
m.gromov <gromoffmaximka@gmail.com>
2024-09-03 14:59:47 +0300

    change header visibility

--- src/js/header.js
+++ src/js/header.js
@@ -1,14 +1,21 @@
-const $header = document.querySelector('.header')
-const activeClass = 'header_bg'
-
-if (window.pageYOffset > 0) {
-    $header.classList.add(activeClass)
-}
-
-window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 0) {
-        $header.classList.add(activeClass)
+const header = document.querySelector('.header')
+const face = document.querySelector('.main')
+const activeHeader = 'header_active'
+const breakPoint = 1024
+const mediaQuery = matchMedia(`(max-width: ${breakPoint}px)`)
+const setOpacity = () => window.scrollY > face.clientHeight ?
+    header.classList.add(activeHeader) :
+    header.classList.remove(activeHeader)
+const checkWidth = () => {
+    const isMobile = window.innerWidth <= breakPoint
+    if (isMobile) {
+        header.classList.add(activeHeader)
+        window.removeEventListener('scroll', setOpacity, { passive: true })
         return
     }
-    $header.classList.remove(activeClass)
-})
+    header.classList.remove(activeHeader)
+    window.addEventListener('scroll', setOpacity, { passive: true })
+}
+
+checkWidth()
+mediaQuery.addEventListener('change', checkWidth)
